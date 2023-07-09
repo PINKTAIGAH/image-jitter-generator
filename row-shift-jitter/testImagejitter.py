a3c5f27025e37d57bcf41968e08df54e6cad0666
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-07-09 23:25:07 +0100

    tested image generation class with jitter filter. Working perfectly

--- row-shift-jitter/testImagejitter.py
+++ row-shift-jitter/testImagejitter.py
@@ -1,15 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from jitterFilter import JitterFilter
+from imageGenerator import ImageGenerator
 
-N = 10
-testImage = np.arange(0, N**2).reshape(N,N)
+N = 64 
+testImage = ImageGenerator(N).generateLines(6) 
 
 testClass = JitterFilter(testImage, N, 3)
 jitterImage = testClass.linearJitter()
 testClass.printJitterVector()
 
-fig, (ax1, ax2) = plt.subplots(2,1)
+fig, (ax1, ax2) = plt.subplots(1,2)
 
 ax1.imshow(testImage, cmap="gnuplot")
 ax2.imshow(jitterImage, cmap="gnuplot")
