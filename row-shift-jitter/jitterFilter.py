f512717e0d37974bfd1bee68c7693013c3eb4153
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-07-09 19:11:49 +0100

    added a class for linear jitter

---
+++ row-shift-jitter/jitterFilter.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+class JitterFilter(object):
+
+    def __init__(self, array, hight, jitterRadius):
+        self.array = array
+        self.hight = hight
+        self.jitterRadiusList = np.arange(-jitterRadius, jitterRadius+1)
+    
+    def linearJitter(self):
+        arrayCopy = np.copy(self.array)
+        self.jitterVector = np.random.choice(self.jitterRadiusList, size=self.hight)
+
+        for idx in range(self.hight):
+            arrayCopy[idx] = np.roll(arrayCopy[idx], self.jitterVector[idx])
+        return arrayCopy 
+
+    def printJitterVector(self):
+        print(self.jitterVector)
+
+if __name__ == "__main__":
+    N = 5
+    testArray = np.arange(0, 5**2).reshape(N,N)
+    testClass = JitterFilter(testArray, N, 1)
+    jitteredArray = testClass.linearJitter()
+
+    print(testArray)
+    print('/' * 20)
+    print(jitteredArray)
