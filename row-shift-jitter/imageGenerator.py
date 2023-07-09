e5600266859570674eb66c918d6c3bd2d5fd0ee7
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-07-10 00:42:42 +0100

    added method to generate a random image from the mnist dataset

--- row-shift-jitter/imageGenerator.py
+++ row-shift-jitter/imageGenerator.py
@@ -1,5 +1,7 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
+from PIL import Image
 from skimage.filters import gaussian
 from scipy.signal import convolve2d
 
@@ -10,7 +12,7 @@ class ImageGenerator(object):
     def generateLines(self, nLines=3):
         self.array = np.zeros((self.N, self.N))
         for _ in range(nLines):
-            randomInt = np.random.random_integers(0, self.N-1)
+            randomInt = np.random.randint(0, self.N-1)
             self.array[:, randomInt-1] = 1
             self.array[:, randomInt] = 1
             self.array[:, randomInt+1] = 1
@@ -25,15 +27,22 @@ class ImageGenerator(object):
         for _ in range(nConvolutions):
             self.array = convolve2d(self.array, self.kernal, boundary="wrap")
         return self.array 
-            
 
     def createKernalPSF(self):
         self.kernal = np.zeros((self.kernalSize, self.kernalSize))
         self.kernal[self.kernalSize//2, self.kernalSize//2] = 1 
         self.kernal = gaussian(self.kernal)
 
+    def generateRandomMnist(self, root="/media/giorgio/HDD/GAN/dcgan/mnist_dataset/"):
+        nImages = len(os.listdir(root))
+        randomImage = os.listdir(root)[np.random.randint(0, nImages-1)]
+        image = Image.open(root + randomImage)
+        imageArray = np.asarray((image))
+        return imageArray
+        
+
 if __name__ == "__main__":
-    cmap = "gnuplot"
+    cmap = "gray"
     testClass = ImageGenerator()
-    plt.imshow(testClass.generateGenericNoise(), cmap=cmap)
+    plt.imshow(testClass.generateRandomMnist(), cmap=cmap)
     plt.show()
