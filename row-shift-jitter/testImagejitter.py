e86bb99c6ef8ff500bb40094e08e8b7ae51a6fe4
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-07-10 00:45:36 +0100

    added test for mnist dataset

--- row-shift-jitter/testImagejitter.py
+++ row-shift-jitter/testImagejitter.py
@@ -5,7 +5,8 @@ from imageGenerator import ImageGenerator
 
 cmap = "gray"
 N = 64 
-testImage = ImageGenerator(N).generateGenericNoise(nConvolutions=3, kernalSize=8) 
+testImage = ImageGenerator(N).generateRandomMnist() 
+N = testImage.shape[0]
 
 testClass = JitterFilter(testImage, N, 3)
 jitterImage = testClass.linearJitter()
