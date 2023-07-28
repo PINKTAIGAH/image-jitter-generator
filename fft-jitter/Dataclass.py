37462d570af5cab26604443a0615c652363ac813
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-07-28 16:09:03 +0100

    added method to convolute using fft of kernal

--- fft-jitter/Dataclass.py
+++ fft-jitter/Dataclass.py
@@ -1,1 +1,34 @@
-from torch.utils.data import Dataclass
+from torch.utils.data import Dataset
+import torch
+import config
+import utils
+import matplotlib.pyplot as plt
+
+def generate_ground_truth(ft_psf,):
+    white_noise = torch.randn(*ft_psf.shape)
+    ground_truth = torch.fft.ifft2(ft_psf * torch.fft.fft2(white_noise))  
+    return torch.real(ground_truth), white_noise
+
+def generate_shifts(max_jitter, image_hight):
+    return torch.randn(image_hight-1)*max_jitter
+
+
+def test():
+
+    ft_psf = torch.fft.fft2(config.PSF)
+    ground_truth, unconvoluted = generate_ground_truth(ft_psf)
+
+    shifts = generate_shifts(config.MAX_JITTER, config.IMAGE_SIZE)
+    print(shifts.shape)
+
+    # fig, (ax1, ax2) = plt.subplots(1, 2)
+
+    plt.imshow(ground_truth)
+    plt.colorbar()
+    # ax2.imshow(unconvoluted)
+    # plt.colorbar()
+    plt.show()
+    
+
+if __name__ == "__main__":
+    test()
--- fft-jitter/config.py
+++ fft-jitter/config.py
@@ -3,8 +3,9 @@ import numpy as np
 import torch
 import utils
 
-IMAGE_SIZE = 128
-SIGMA = 3
+IMAGE_SIZE = 256
+SIGMA = 7 
+MAX_JITTER = 4
 
 kernal = np.zeros((IMAGE_SIZE, IMAGE_SIZE))
 kernal[IMAGE_SIZE//2, IMAGE_SIZE//2] = 1
