87fbbc4cfb7ace297fc0f708ac27fb3c09814618
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-08-07 11:56:33 +0100

    Added complete calculation of shift vectors including parameters for modification

--- harmonic/harmonic_jitter.py
+++ harmonic/harmonic_jitter.py
@@ -1,11 +1,12 @@
 from kornia.geometry.transform import warp_perspective
+from scipy.ndimage import shift
 from ImageGenerator import ImageGenerator
 import config
 import utils
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
-
+from time import time
 
 def generateRandomFrequency(mode="m"):
     match mode:
@@ -41,28 +42,40 @@ def message(x):
     f_h, A_h, P_h = generateRandomFrequency("h"), generateRandomAmplitude(5), generateRandoPhase()
     return A_m*np.sin(2*np.pi*f_m*x+P_m) + A_l*np.sin(2*np.pi*f_l*x+P_l) + A_h*np.sin(2*np.pi*f_h*x+P_h)
 
-def carrier(x):
+def carrier(x, image_size):
+    p = generateRandoPhase()
     f = generateRandomFrequency('l')
-    return np.sin(f*np.pi*x/256)
+    return np.sin(f*np.pi*x/image_size + p)
 
 def decay(x, x_0=0.0, std=1.0):
     return np.exp(-(x-x_0)**2/(2*std**2))
 
-CORRELATION_LENGTH = 4
-MAX_JITTER =  5
-wavelet_centers = np.arange(0, 256, CORRELATION_LENGTH*3)
-x = np.arange(256)
-y_final = np.zeros_like(x, dtype=np.float64)
+t1 = time()
 
-for i, val in enumerate(wavelet_centers):
-    y = message(x)
-    y_decay = decay(x, val, CORRELATION_LENGTH)
-    y_carrier = carrier(x)
-    y_final += utils.adjustArray(y * y_decay * y_carrier)*MAX_JITTER
+
+IMAGE_SIZE = 256
+CORRELATION_LENGTH = 5
+MAX_JITTER =  5
+shift_vector = np.empty((IMAGE_SIZE, IMAGE_SIZE))
+wavelet_centers = np.arange(0, IMAGE_SIZE, CORRELATION_LENGTH*3)
+
+for j in range(IMAGE_SIZE):
+    x = np.arange(IMAGE_SIZE)
+    y_final = np.zeros_like(x, dtype=np.float64)
+    for i, val in enumerate(wavelet_centers):
+        y = message(x)
+        y_decay = decay(x, val, CORRELATION_LENGTH)
+        y_carrier = carrier(x, IMAGE_SIZE)
+        y_final += utils.adjustArray(y * y_decay * y_carrier)*MAX_JITTER
+    shift_vector[j] = y_final
+t2 = time()
+print(f"time taken per image is {t2 - t1} s")
+
+print(shift_vector)
 
 fig, (ax1, ax2) = plt.subplots(2, 1)
-ax1.scatter(x, y_final, s=5, marker='x')
-ax2.plot(x, y_final)
+ax1.scatter(x, shift_vector[0], s=5, marker='x')
+ax2.plot(x, shift_vector[0])
 plt.show()
     
 
